da717e04db6c43f5f1afd012c941e25216f53ed5
Joris van der Vorst <joris@jvandervorst.nl>
2020-01-08 17:11:38 +0100

    Create seasonality class to scale amount of messages

--- Seasonality/Seasonality_adjustment.py
+++ Seasonality/Seasonality_adjustment.py
@@ -4,16 +4,41 @@ import pandas as pd
 # time = input("Enter time in seconds")
 time_value_1 = 150000
 time_value_2 = 20000
-import os
-print(os.getcwd())
 
-filename = "Seasonality/seasonality_values.csv"
-seasonality_df = pd.read_csv(filename, sep = ";")
-max_time_seasonality = max(seasonality_df["time"].values)
-def seasonality_scalar(seasonality_dataframe, timestamp):
-    timestamp = timestamp % max_time_seasonality
-    closest_time = abs(seasonality_dataframe["time"]-timestamp).values.argmin()
-    return seasonality_dataframe["amount"][closest_time]
+class Seasonality(object):
+    """ Seasonality adjust the maximum transaction rate according to a specified .csv
+    file containing a scaler for certain time stamps
+    requires pandas as pd and numpy as np
+    """
+    def __init__(self, seasonality_file, envoirment = None):
+        self.seasonality_file = seasonality_file
+        self.env = envoirment
 
-print(f"""Time: {time_value_1}, seasonality scalar {seasonality_scalar(seasonality_df, time_value_1)}""")
-print(f"""Time: {time_value_2}, seasonality scalar {seasonality_scalar(seasonality_df, time_value_2)}""")
+        # Import seasonality .csv file
+        self.seasonality_df = pd.read_csv(self.seasonality_file , sep = ";")
+
+        # Find highest time value in seasonality seasonality_dataframe
+        self.max_time_seasonality = max(seasonality_df["time"].values)
+
+    def scale(self, timestamp = None):
+        """ Return scalar to adjust amount of messages, use timestamp if given,
+        otherwise call envoirment to determine current time
+        """
+
+        # If no timestamp is given, use Simpy envoirment to get time
+        if timestamp is None:
+            if self.env is None:
+                raise BaseException("No timestamp or envoirment specified")
+            timestamp = self.env.now
+
+        # Loop if timestamp is larger than max seasonality time
+        timestamp = timestamp % self.max_time_seasonality
+        # Find time value closest to timestamp
+        closest_time = abs(self.seasonality_df["time"]-timestamp).values.argmin()
+        # Return scaler value correspoding to closest_time
+        return self.seasonality_df["scaler_value"][closest_time]
+
+season = Seasonality(filename)
+
+print(f"""Time: {time_value_1}, seasonality scalar {season.scale(time_value_1)}""")
+print(f"""Time: {time_value_2}, seasonality scalar {season.scale(time_value_2)}""")
