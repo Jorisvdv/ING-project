dbec1f586029d0f58e85b1ecc59f62b75c289eca
Antonio Javier Samaniego <samaniego821@gmail.com>
2020-01-14 11:50:20 +0100

    Add files via upload

---
+++ app2/client.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+Class for starting up a webclient, which can be used as frontend for the
+simulation.
+
+@author Tycho Atsma <tycho.atsma@gmail.com>
+@file   client.py
+@scope  public
+"""
+
+# dependencies
+from flask import Flask
+import routes
+
+class Client(object):
+
+    def __init__(self, config):
+        """
+        Constructor.
+
+        Parameters
+        ----------
+        config: dict
+            Configuration for the flask application.
+        """
+        # we need to construct a flask application, which will be the
+        # backbone of the web client
+        self._client = Flask(__name__, static_url_path='', static_folder="web/static", template_folder="web/templates")
+
+        # we need to setup some configuration variables, these may need to change
+        # when running in production
+        self._client.config.update(config)
+
+        # we need to install all routes onto the client
+        routes.install(self._client)
+
+    def run(self):
+        """
+        Method to run the client. This will start up a simple webserver.
+        """
+        # run the client
+        self._client.run()
+
+
+# run as main
+if __name__ == "__main__":
+
+    # run a new client
+    Client({
+        'TESTING':  True,
+        'ENV':     'development',
+        'DEBUG':    True
+    }).run()
